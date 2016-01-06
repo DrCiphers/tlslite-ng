cad2872dd1ab411f0d9dbc81dceb19127bdca8a0
E.Iosifidis <iosifidise@gmail.com>
2016-01-06 13:59:00 +0200

    Optimization improvements
    Optimization improvements in function numberToByteArray

--- tlslite/utils/python_speck.py
+++ tlslite/utils/python_speck.py
@@ -43,8 +43,7 @@ class Python_SPECK():
 
         # Pre-compile key schedule
         self.key_schedule = [self.key & self.mod_mask]
-        l_schedule = [(self.key >> (x * self.word_size)) & self.mod_mask for x in
-                      xrange(1, 128 // self.word_size)]
+        l_schedule = [(self.key >> (x * self.word_size)) & self.mod_mask for x in xrange(1, 128 // self.word_size)]
 
         for x in xrange(self.rounds - 1):
             new_l_k = self.encrypt_round(l_schedule[x], self.key_schedule[x], x)
@@ -87,17 +86,15 @@ class Python_SPECK():
         return total
 
 
-    def numberToByteArray(self,n, howManyBytes=None):
-        """Convert an integer into a bytearray, zero-pad to howManyBytes.
+    def numberToByteArray(self,n):
+        """Convert an integer into a bytearray, zero-pad to 16 Bytes.
     
         The returned bytearray may be smaller than howManyBytes, but will
         not be larger.  The returned bytearray will contain a big-endian
         encoding of the input integer (n).
         """    
-        if howManyBytes == None:
-            howManyBytes = numBytes(n)
-        b = bytearray(howManyBytes)
-        for count in xrange(howManyBytes-1, -1, -1):
+        b = bytearray(16)
+        for count in xrange(15, -1, -1):
             b[count] = int(n % 256)
             n >>= 8
         return b
@@ -128,44 +125,45 @@ class Python_SPECK():
 
         return new_x, new_y
    
-  
+   
     
     def encrypt(self, plaintext):        
         
         plaintextBytes = plaintext[:]
         chainBytes = self.IV[:]      
         
-        
+
         #CBC Mode: For each block...
         for x in xrange(len(plaintextBytes)//16):
-            
+
             #XOR with the chaining block
             blockBytes = plaintextBytes[x*16 : (x*16)+16]
-            
+        
+                
             for y in range(16):
                 blockBytes[y] ^= chainBytes[y]
             
             
+           
             blockBytesNum = self.bytesToNumber(blockBytes)
+
 
             mod_mask = 18446744073709551615L
             
             b = (blockBytesNum >> self.word_size) & mod_mask
             a = blockBytesNum & mod_mask      
             
-            #start_time = time.time()
-            
+           
             keylist = self.key_schedule
-            
-            for i in keylist:
-                b, a = self.encrypt_round(b, a, i)  
-  
-            #print("--- SPECK Encryption Block %d %s seconds ---" %(block_no , (time.time() - start_time)))
-  
-  
+
+            for i in self.key_schedule:
+                b, a = self.encrypt_round(b, a, i) 
+                del i
+                
             ciphertext = (b << self.word_size) | a                             
-            ciphertext= self.numberToByteArray(ciphertext,howManyBytes=16) 
-                       
+            ciphertext= self.numberToByteArray(ciphertext) 
+            
+                        
             #Overwrite the input with the output
             for y in xrange(16):
                 plaintextBytes[(x*16)+y] = ciphertext[y]
@@ -196,15 +194,13 @@ class Python_SPECK():
         
             b = (ciphertext >> self.word_size) & mod_mask
             a = ciphertext & mod_mask       
-
-            keylist_rev = reversed(self.key_schedule)
            
-            for i in keylist_rev:
+            for i in reversed(self.key_schedule):
                 b, a = self.decrypt_round(b, a, i)
           
             plaintext = (b << self.word_size) | a    
                 
-            plaintext = self.numberToByteArray(plaintext,howManyBytes=16)  
+            plaintext = self.numberToByteArray(plaintext)  
             
             #XOR with the chaining block and overwrite the input with output
             for y in xrange(16):

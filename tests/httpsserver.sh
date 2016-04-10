872255d624d01c9232c371a6928ed2222f7b1c12
E.Iosifidis <iosifidise@gmail.com>
2016-04-10 19:38:47 +0300

    Update httpsserver.sh

--- tests/httpsserver.sh
+++ tests/httpsserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "$1" ]; then
-        python ../scripts/tls.py server -k serverX509Key.pem -c serverX509Cert.pem -t TACK1.pem localho$
+        python ../scripts/tls.py server -k serverX509Key.pem -c serverX509Cert.pem -t TACK1.pem localhost:4443
 else
         python ../scripts/tls.py server -k serverX509Key.pem -c serverX509Cert.pem -t TACK1.pem $1:4443
 fi
